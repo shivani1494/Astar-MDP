50e6400c877273b8c3f5ee416c7d4328a2b42f5e
Shivani <shivanimall1494@gmail.com>
2016-05-27 09:55:40 -0500

    errors removed

--- scripts/astar.py
+++ scripts/astar.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import sys
 import rospy
 import math as m
@@ -8,20 +7,22 @@ from copy import deepcopy
 from read_config import read_config
 from std_msgs.msg import String, Float32, Bool
 from cse_190_assi_3.msg import AStarPath, PolicyList
-import heapq as h 
-#astar implementation
+import heapq as h
 
 class RAstar:
 	def __init__(self):
 		self.config = read_config()
 		self.resPub = rospy.Publisher("/results/path_list", AStarPath, queue_size=10)
-		self.resPub.publish([1,1])	
+		astarp = AStarPath()
+		astarp.data = [0, 0]
+		#print ("are you publishing??")
+		self.resPub.publish(astarp)		
 		self.row = self.config["map_size"][0]
 		self.column = self.config["map_size"][1]
-		print ("self.row")
-		print self.row
-		print ("self.column")
-		print self.column
+		#print ("self.row")
+		#print self.row
+		#print ("self.column")
+		#print self.column
 
 		self.tempRow = []
 		self.mmap = []
@@ -31,182 +32,173 @@ class RAstar:
 
 		for j in range( (self.row)):
 			self.mmap.append(deepcopy(self.tempRow) ) 
-		
-		print ("self.mmap")
-		print self.mmap
+	
+		#m = deepcopy(self.mmap)
+		#print np.reshape(m, (self.row, self.column) )	
+		#print ("self.mmap")
+		#print len (self.mmap)
+		#print len (self.mmap[0])
+		#print "aaA"
+		#print  ("self.mmap")
+		#print  (self.mmap)
 
 		self.start = self.config["start"]
 		self.goal = self.config["goal"]
-		self.goalX = self.goal[0]
-		self.goalY = self.goal[1]
-		self.startX = self.start[0]
-		self.startY = self.start[1]
+		self.goal_r = self.goal[0]
+		self.goal_c = self.goal[1]
+		self.start_r = self.start[0]
+		self.start_c = self.start[1]
 		self.pits = self.config["pits"]
 		self.walls = self.config["walls"]
-
 		self.openList = set([])
-		self.closedList = set([])		
 		self.AStar()
 
+	def is_Wall_Pit_InMap(self, curr_r, curr_c):
+		c_Bound = self.column - 1
+		r_Bound = self.row - 1
+		if curr_r < 0 or curr_c < 0 or curr_r > r_Bound or curr_c > c_Bound: 
+			#print "out of bounds: curr row and column"
+			#print curr_r
+			#print curr_c
+			return False
+		#checking for pits	
+		for i in range( len(self.pits) ):
+			if self.pits[i][0] == curr_r and self.pits[i][1] == curr_c:
+				return False
+		#checking for walls
+		for i in range( len(self.walls) ):
+			if self.walls[i][0] == curr_r and self.walls[i][1] == curr_c:
+				return False
+		return True
+
+	def initializeMap(self):
+		for j in range( len(self.mmap) ):
+			for i in range( len(self.mmap[j])  ):
+				hrstic = abs(self.goal_c - i) + abs(self.goal_r - j)
+				backC = sys.maxint
+				f_hb = hrstic + backC
+				parent = None
+				
+				
+				if j == self.start_r and i == self.start_c:
+					#print "entering for the start case"
+					#print j
+					#print i
+					self.mmap[j][i][0] = 0 
+					self.mmap[j][i][1] = 0 
+					self.mmap[j][i][2] = 0
+					self.mmap[j][i][3] = parent
+						
+				elif j == self.goal_r and i == self.goal_c:
+					self.mmap[j][i][0] = 0 
+					self.mmap[j][i][1] = backC 
+					self.mmap[j][i][2] = backC 
+					self.mmap[j][i][3] = parent
+				
+				else:		
+					self.mmap[j][i][0] = hrstic 
+					self.mmap[j][i][1] = backC 
+					self.mmap[j][i][2] = f_hb
+					self.mmap[j][i][3] = parent
+		#print self.mmap
 
 	def addToOpenList(self, currX, currY):
 		strCurr = str(currX) + str(currY)
 		self.openList.add(strCurr)	
-
-	def addToClosedList(self, currX, currY):	
-		strCurr = str(currX) + str(currY)
-		self.closedList.add(strCurr)	
-
 	def isInOpenList(self, currX, currY):
 		strCurr = str(currX) + str(currY)
 		if strCurr in self.openList:
-			return true
-		return false
-
-	def isInClosedList(self, currX, currY):
+			return True
+		return False
+	def removeFrOpenList(self, currX, currY):
 		strCurr = str(currX) + str(currY)
-		if strCurr in self.closedList: 
-			return true
-		return false
-
-	def is_Wall_Pit_InMap(self, currX, currY):
-		xBound = self.column - 1
-		yBound = self.row - 1
-		if currX < 0 or currY < 0 or currX > xBound or currY > yBound: 
-			return false
-		#checking for pits	
-		for i in range( len(self.pits) ):
-			if self.pits[i][0] == currX and self.pits[i][1] == currY:
-				return false
-		#checking for walls
-		for i in range( len(self.walls) ):
-			if self.walls[i][0] == currX and self.walls[i][1] == currY:
-				return false
-		return true
-
-	def initializeMap(self):
-		for i in range( len(self.mmap) ):
-			for j in range( len(self.mmap[i]) ):
-				hrstic = abs(self.goalY - j) + abs(self.goalX - i)
-				backC = sys.maxint
-				f_hb = hrstic + backC
-				zero = 0
-				parent = None
-				if i == self.startX and j == self.startY:
-					self.mmap[i][j][0] = hrstic
-					self.mmap[i][j][1] = zero 
-					self.mmap[i][j][2] = hrstic
-					self.mmap[i][j][3] = parent
-				
-				if i == self.goalX and j == self.goalY:
-					self.mmap[i][j][0] = zero
-					self.mmap[i][j][1] = backC 
-					self.mmap[i][j][2] = backC 
-					self.mmap[i][j][3] = parent
-				
-				else:		
-					self.mmap[i][j][0] = hrstic 
-					self.mmap[i][j][1] = backC 
-					self.mmap[i][j][2] = f_hb
-					self.mmap[i][j][3] = parent
-				#this is a tuple of all the values we keep track of
-		print ("self.mmap")
-		print self.mmap
+		self.openList.remove(strCurr)
 	
 	def AStar(self):	
 		#set all the initial values
 		self.initializeMap()
 		#keep a track of all the tentative paths
 		allPaths = []
-	
 		#start = [ 7, '30'] 
 		start = []
-		start.append(self.mmap[self.startX][self.startY][2])
-		start.append(str(self.startX)+ str(self.startY) )	
-
+		start.append(self.mmap[self.start_r][self.start_c][2])
+		start.append(str(self.start_r)+ str(self.start_c) )	
+		self.addToOpenList(self.start_r, self.start_c)
 		allPaths.append(start)
 		h.heapify(allPaths)
-
-		#does the map wrap around?	
-		while not allPaths:		
+		
+		while allPaths:		
 			curr = h.heappop(allPaths)	
-			
 			temp = list(curr[1])			
-			currX = int(temp[0])
-			currY = int(temp[1])
+			curr_r = int(temp[0])
+			curr_c = int(temp[1])
 			suc = []
-			#goal test 
-			if mmap[currX][currY] == mmap[self.goalX][self.goalY]:
-				print ("goal found")
+			self.removeFrOpenList(curr_r, curr_c)
+			#print "curr row and column"
+			#print curr_r
+			#print curr_c
+			if curr_c == self.goal_c and curr_r == self.goal_r:
+				#print ("goal found")
 				oPath = []
-				corX = currX
-				corY = currY
-				pathC = true
+				rw = curr_r
+				clm = curr_c
+				pathC = True
 				while pathC:
-					corP = mmap[corX][corY][3]
-					oPath.append([corX, corY])
-					if corP is not None:
-						pathC = false
+					parentL = self.mmap[rw][clm][3]
+					oPath.append([rw, clm])
+					if parentL is None:
+						pathC = False
 						break
 
-					tempL = list(corP)			
-					corX = int(temp[0])
-					corY = int(temp[1])
+					rw = parentL[0]
+					clm = parentL[1]
 				
-				#reverse oPath
 				optimalPath = list(reversed(oPath))
-				'''#find out the steps the robot will have to take based on
-				self.moves = []
-				for i in range( len (oPath) - 1):
-					#either x has to be equal
-					if oPath[i][0] == oPath[i+1][0]:
-						if oPath[i][1] + 1 == oPath[i+1][1]:
-							moves.append([0,1])
-						elif oPath[i][1] - 1 == oPath[i+1][1]:	
-							moves.append([0,-1])
-
-					#or y has to be equal
-					elif oPath[i][1] == oPath[i][1]:
-						if oPath[i][0] + 1 == oPath[i+1][0]:
-							moves.append([1,0])
-						elif oPath[i][0] - 1 == oPath[i+1][0]:	
-							moves.append([-1,0])
-				'''
-				#that and then publish it
+				print optimalPath	
+				#print optimalPath
 				for i in range(len(optimalPath)):
-					self.resPub.publish(optimalPath[i])		
+					#print "optimalPath[i]"
+					#print optimalPath[i]
+					astar_path = AStarPath()
+					astar_path.data = optimalPath[i]
+					self.resPub.publish(astar_path)		
 		
 				break
-				#are you sure you want to break?
 			else:
-
-				#find out all the neighbors
-				#these will be the successors		
-				if self.is_Wall_Pit_InMap(currX+1, currY):
-					suc.append( [currX+1, currY] )
-				if self.is_Wall_Pit_InMap(currX-1, currY):
-					suc.append( [currX-1, currY] )
-				if self.is_Wall_Pit_InMap(currX, currY+1):
-					suc.append( [currX, currY+1] )
-				if self.is_Wall_Pit_InMap(currX, currY-1):
-					suc.append( [currX, currY-1] )
-				
+				if self.is_Wall_Pit_InMap(curr_r+1, curr_c):
+					suc.append( [curr_r+1, curr_c] )
+				if self.is_Wall_Pit_InMap(curr_r-1, curr_c):
+					suc.append( [curr_r-1, curr_c] )
+				if self.is_Wall_Pit_InMap(curr_r, curr_c+1):
+					suc.append( [curr_r, curr_c+1] )
+				if self.is_Wall_Pit_InMap(curr_r, curr_c-1):
+					suc.append( [curr_r, curr_c-1] )
+				#print "succesor:"
+				#print suc		
 				for i in range (len (suc) ):
-					suc[i][0] = ncurrX
-					suc[i][1] = ncurrY	
-
-					if self.isNewNode(ncurrX, ncurrY):
-						self.mmap[ncurrX][ncurrY][3] = [currX, currY]
-						addToOpenList(ncurrX, ncurrY)
-						tempAdd = [ mmap[ncurrX][ncurrY][2] ]
-						tempAdd.append(str(ncurrX)+ str(ncurrY))	
+					ncurr_r = suc[i][0]
+					ncurr_c = suc[i][1]
+					
+					setParam = False
+					if self.mmap[ncurr_r][ncurr_c][1] == sys.maxint:
+						self.addToOpenList(ncurr_r, ncurr_c)
+						#print "new row_column to be added"
+						#print ncurr_r
+						#print ncurr_c
+						#print "self.mmap[ncurr_r][ncurr_c]"	
+						#print self.mmap[ncurr_r][ncurr_c]	
+						setParam = True
+		
+					elif self.isInOpenList(ncurr_r, ncurr_c):
+						if self.mmap[ncurr_r][ncurr_c][1] > self.mmap[curr_r][curr_c][1] + 1:
+							print "did we ever enter into this one?"	
+							setParam = True
+					if setParam:							
+						self.mmap[ncurr_r][ncurr_c][3] = [curr_r, curr_c]
+						self.mmap[ncurr_r][ncurr_c][1] = 1 + self.mmap[curr_r][curr_c][1]
+						self.mmap[ncurr_r][ncurr_c][2] = self.mmap[ncurr_r][ncurr_c][0] + self.mmap[ncurr_r][ncurr_c][1]
+						tempAdd = [ self.mmap[ncurr_r][ncurr_c][2] ]
+						tempAdd.append(str(ncurr_r)+ str(ncurr_c))	
 						h.heappush(allPaths, tempAdd) 
-					#this case will never happen?
-					elif self.isInOpenList(ncurrX, ncurrY):
-						if self.mmap[ncurrX][ncurrY][1] > self.mmap[currX][currY][1] + 1:
-							self.mmap[ncurrX][ncurrY][3] = [currX, currY]
-							self.mmap[ncurrX][ncurrY][1] = 1 + self.mmap[currX][currY][1]
-							self.mmap[ncurrX][ncurrY][2] = self.mmap[ncurrX][ncurrY][0] + self.mmap[ncurrX][ncurrY][1]
-								
-				#add the current to closed list
-				self.addToClosedList(currX, currY)
+						#print "self.mmap[ncurr_r][ncurr_c]"	
+						#print self.mmap[ncurr_r][ncurr_c]	
--- scripts/mdp.py
+++ scripts/mdp.py
@@ -1,1 +1,135 @@
-# mdp implementation needs to go here
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String, Float32, Bool
+from cse_190_assi_3.msg import AStarPath, PolicyList
+import json
+import image_util
+from read_config import read_config
+
+
+class MDP:
+
+	def __init__(self):
+		self.config = read_config()
+
+		self.probF = self.config["probability_move_forward"]
+		self.probB = self.config["probability_move_backward"]
+		self.probL = self.config["probability_move_left"]
+		self.probR = self.config["probability_move_right"]
+
+
+		self.maxI = self.config["max_iterations"]
+		self.threshDiff = self.config["thershold_difference"]
+		
+		self.stepRwrd = self.config["reward_for_each_step"]
+		self.wallRwrd = self.config["reward_for_hitting_wall"]
+		self.goalRwrd = self.config["reward_for_reaching_goal"]
+		self.pitRwrd = self.config["reward_for_falling_in_pit"]
+
+                #self.resPub = rospy.Publisher("/results/path_list", AStarPath, queue_size=10)
+                #self.resPub.publish([1,1])
+                self.row = self.config["map_size"][0]
+                self.column = self.config["map_size"][1]
+    
+                self.tempRow = []
+                self.mmap1 = []
+                #creating the map 
+                for i in range( (self.column) ):
+                        self.tempRow.append([0 , 0 , 0, 0])
+
+                for j in range( (self.row)):
+                        self.mmap1.append(deepcopy(self.tempRow) )
+
+                self.mmap2 = []
+
+                for j in range( (self.row)):
+                        self.mmap2.append(deepcopy(self.tempRow) )
+
+                self.start = self.config["start"]
+                self.goal = self.config["goal"]
+                self.goal_r = self.goal[0]
+                self.goal_c = self.goal[1]
+                self.start_r = self.start[0]
+                self.start_c = self.start[1]
+                self.pits = self.config["pits"]
+                self.walls = self.config["walls"]
+	
+		self.drctns = ["N", "S", "E", "W"]
+		
+	def northMoves(self, r, c, ):
+		
+        
+	def is_InMap(self, curr_r, curr_c):
+                c_Bound = self.column - 1
+                r_Bound = self.row - 1
+                if curr_r < 0 or curr_c < 0 or curr_r > r_Bound or curr_c > c_Bound:
+                        return False
+                return True
+	
+	def is_Wall(self, curr_r, curr_c):
+                #checking for walls
+                for i in range( len(self.walls) ):
+                        if self.walls[i][0] == curr_r and self.walls[i][1] == curr_c:
+                                return True
+		return False 
+	
+	def is_Pit(self, curr_r, curr_c):
+                #checking for pits      
+                for i in range( len(self.pits) ):
+                        if self.pits[i][0] == curr_r and self.pits[i][1] == curr_c:
+                                return True 
+		return False
+
+	def MDP_Algo(self):
+		
+		self.mmap1[self.goal_r][self.goal_c] = self.goalRwrd	
+		self.mmap2[self.goal_r][self.goal_c] = self.goalRwrd	
+
+		for i in range(len(self.walls) ):
+			wallX = self.walls[i][0]	
+			wallY = self.walls[i][1]	
+			self.mmap1[wallY][wallX] = self.wallRwrd	
+			self.mmap2[wallY][wallX] = self.wallRwrd	
+				
+		for i in range(len(self.pits) ):
+			pitX = self.pits[i][0]	
+			pitY = self.pits[i][1]	
+			self.mmap1[pitY][pitX] = self.pitRwrd	
+			self.mmap2[pitY][pitX] = self.pitRwrd	
+
+		for r in range ( len (self.mmap) ):
+			
+			for c in range ( len (self.mmap[i]) ):
+					#forward   backward   left      right 
+				#north  r+1, c+0   r-1, c+0   r+0, c-1  r+0, c+1
+				#south  r-1, c+0   r+1, c+0   r+0, c+1  r+0, c-1
+				#west	r, c-1     r, c+1     r-1,c     r+1, c
+				#east   r, c+1     r, c-1     r+1,c     r-1, c 
+	
+				#prob: forward backward left right
+				for a in range( len ( self.actions ) ):
+					for d in range(len(self.actions[a]) ):
+
+					if isGoal(r + act[a][d][0], c + act[a][d][1] ):
+						#prev_reward + goalRwrd					
+						reward = self.mmap1[r+act[a][d][0]][c+act[a][d][1]] + goalRwrd
+						nlocP = [ [r+act[a][d][0], c+act[a][d][1]], self.probAct[d] ]
+					
+					elif self.is_Pit(r + act[a][d][0], c + act[a][d][1]):
+						
+						reward = self.mmap1[r+act[a][d][0]][c+act[a][d][1]] + pitRwrd
+						nlocP = [ [r+act[a][d][0], c+act[a][d][1]], self.probAct[d] ]
+							
+					elif self.is_Wall(r + act[a][d][0], c + act[a][d][1]):
+						#stay put, so get the prev reward of staying put + wallRwrd
+						reward = self.mmap1[r][c] + wallRwrd
+						nlocP = [ [r, c], self.probAct[d] ]
+					elif self.is_InMap(r + act[a][d][0], c + act[a][d][1]):
+
+						nlocP = [ [r+act[a][d][0], c+act[a][d][1]], self.probAct[d] ]
+						reward = stepRwrd
+					elif not self.is_InMap(r + act[a][d][0], c + act[a][d][1]):
+	
+						nlocP = [ [r, c], self.probAct[d] ]
+						reward = stepRwrd
